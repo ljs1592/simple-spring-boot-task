0bee037dbcc5dfb59305c6a40925b25d3205b9cd
21Days <15922692@qq.com>
2018-05-10 09:38:22 +0800

    修改部分注释和删除多余属性

--- src/main/java/com/jack/task/bean/TaskInfoBean.java
+++ src/main/java/com/jack/task/bean/TaskInfoBean.java
@@ -1,6 +1,5 @@
 package com.jack.task.bean;
 
-import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -12,14 +11,36 @@ public class TaskInfoBean {
 
 	private String taskId;
 	
+	/**
+	 * 定时任务名称
+	 */
 	private String taskName;
 	
+	/**
+	 * 定时任务调用服务名
+	 */
 	private String serviceName;
 	
-//	private String serviceMethod;
-	
-	private Date nextStartTime;
-	
+	/**
+	 * 定时任务触发cron表达式
+	 */
+	//第一位,表示秒,取值0-59
+	//第二位,表示分,取值0-59
+    //第三位,表示小时,取值0-23
+	//第四位,日期天/日,取值1-31
+	//第五位,日期月份,取值1-12
+	//第六位,星期,取值1-7,星期一,星期二...
+	//另外:1表示星期天,2表示星期一
+	//第7为,年份,可以留空,取值1970-2099
+	//(*)星号:可以理解为每的意思,每秒,每分,每天,每月,每年...
+	//(?)问号:问号只能出现在日期和星期这两个位置,表示这个位置的值不确定,每天3点执行,所以第六位星期的位置,
+	//我们是不需要关注的,就是不确定的值,同时:日期和星期是两个相互排斥的元素,通过问号来表明不指定值,比如,1月10日,比如是星期1,
+	//如果在星期的位置是另指定星期二,就前后冲突矛盾了
+	//(-)减号：表达一个范围，如在小时字段中使用“10-12”，则表示从10到12点，即10,11,12
+	//(,)逗号:表达一个列表值，如在星期字段中使用“1,2,4”,则表示星期一,星期二,星期四
+	//(/)斜杠:如：x/y,x是开始值,y是步长,比如在第一位(秒) 0/15就是,从0秒开始,每15秒,最后就是0,15,30,45,60    
+	//另：*/y,等同于0/y
+	//举个例子： 0/5 * * * * ?  即为从第0秒开始，每5秒运行一次。
 	private String cronExpression;
 	
 	
@@ -56,22 +77,6 @@ public class TaskInfoBean {
 
 	public void setServiceName(String serviceName) {
 		this.serviceName = serviceName;
-	}
-
-//	public String getServiceMethod() {
-//		return serviceMethod;
-//	}
-//
-//	public void setServiceMethod(String serviceMethod) {
-//		this.serviceMethod = serviceMethod;
-//	}
-
-	public Date getNextStartTime() {
-		return nextStartTime;
-	}
-
-	public void setNextStartTime(Date nextStartTime) {
-		this.nextStartTime = nextStartTime;
 	}
 
 	public String getCronExpression() {
